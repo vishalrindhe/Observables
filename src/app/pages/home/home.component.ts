20d74078e29049b88a23a4af411e700420e74df6
vishalrindhe <rindhevishal32@gmail.com>
2023-10-15 18:08:55 +0530

    create simple observable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  myObeservable = new Observable(observable => {
+    console.log(observable);
+    observable.next('1')
+    observable.next('2')
+    observable.next('3')
+    observable.next('4')
+    observable.next('5')
+    
+  })
   constructor() { }
 
   ngOnInit(): void {
+    this.myObeservable.subscribe(next => {
+      console.log(next);
+    }, error => {
+      console.log(error);
+    }
+    )
   }
 
 }
