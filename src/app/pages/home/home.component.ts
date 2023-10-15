48b335a2f5a2c57c130122827167c709aa4d23a5
vishalrindhe <rindhevishal32@gmail.com>
2023-10-15 20:20:31 +0530

    map, filter, combineLatest

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, from, of } from 'rxjs';
+import { Observable, from, map, filter, of, BehaviorSubject, fromEvent, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,25 +7,41 @@ import { Observable, from, of } from 'rxjs';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  myObeservable =  of([1,2,3,4,5,6],['a','b','c'])
-  // myObeservable = from([1,2,3,4,5,6])
-  // myObeservable = new Observable(observable => {
-  //   observable.next('1')
-  //   // observable.error('error occured message')
-  //   observable.complete()
-  //   observable.next('1')
-  //   observable.error('error occured message')
-  // })
-  // myObeservable = Observable.create((observable:any) => {
-  //   observable.next('1')
-  //   // observable.error('error occured message')
-  //   observable.complete()
-  //   observable.next('1')
-  //   observable.error('error occured message')
-  // })
-    
+  f1 = (value:any) => {
+    return value
+  } 
+  userName = new BehaviorSubject<{id:number,name:string} |null>(null)
+  numArray = [
+    {id : 1, name : 'vishal', isActive : true},  
+    {id : 2, name : 'viraj', isActive : false},  
+    {id : 3, name : 'vikas', isActive : false},  
+    {id : 4, name : 'vaishag', isActive : true} 
+  ]
+  clickOnDOM = fromEvent(document,'click')
+  myObeservable :any = of(this.numArray).pipe(
+    // map((r:any) => r.map((rr:any) => rr)),
+    filter((r:any) => r.filter((rr:any) => rr.isActive)
+    // {
+    //   console.log(r.filter((rr:any) => rr.isActive));
+      
+    //   return r.filter((rr:any) => rr.isActive)
+    //   console.log(r.every((rr:any) => rr.isActive));
+      
+    //   return r.every((rr:any) => rr.isActive)
+    // }
+    )
+  )
 
+  allData = combineLatest([this.myObeservable,this.userName]).pipe(
+    map(([myObservable,userName]:any) =>({
+      myObservable,userName
+    }))
+  )
   ngOnInit(): void {
+    this.allData.subscribe(rr=>{
+      console.log(rr);
+      
+    })
     this.myObeservable.subscribe((next:any) => {
       console.log(next);
     }, (error:any) => {
@@ -35,6 +51,11 @@ export class HomeComponent implements OnInit {
       
     }
     )
+    setTimeout(() => {this.userName.next({id:1,name:'abc'}) }, 3000);
+      this.clickOnDOM.subscribe(rr => {
+        console.log(rr);
+        
+      })
   }
 
 }
